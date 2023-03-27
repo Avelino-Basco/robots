b5c790b9eb021b74ff1b0be807e25ee7598903c9
Avelino-Basco <126433309+Avelino-Basco@users.noreply.github.com>
2023-03-27 13:19:38 +0800

    Update subscriber.py

--- topics/sensors/sensors/subscriber.py
+++ topics/sensors/sensors/subscriber.py
@@ -23,4 +23,13 @@ class SensorsSubscriber(Node):
     self.create_subscription(LaserScan, 'scan', self.scan_callback, qos_profile=qos_profile_sensor_data)
     self.imu_count = 0
 
+  def imu_callback(self, msg):
+    self.get_logger().info('IMU: (qo_x, qo_y, qo_z): (%f,%f,%f) |' % (msg.orientation_covariance.x,msg.orientation_covariance.y,msg.orientation_covariance.z))
+    self.get_logger().info(' (av_x, av_y, av_z): (%f,%f,%f) |' % (msg.angular_velocity_covariance.x,msg.angular_velocity_covariance.y,msg.angular_velocity_covariance.z))
+    self.get_logger().info(' (la_x, la_y, la_z): (%f,%f,%f)\n' % (msg.linear_acceleration_covariance.x,msg.linear_acceleration_covariance.y,msg.linear_acceleration_covariance.z))
+
+  def scan_callback(self, msg):
+    self.get_logger().info('SCAN: max_range = %f | min_range = %f\n' %(msg.range_max,msg.range_min))
     
+  def battery_callback(self, msg):
+    self.get_logger().info('BATTERY: %s | Voltage: %f | Temp: %f | Current: %f' %(msg.present, msg.voltage, msg.temperature, msg.current))
